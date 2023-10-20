2454ac88cdf457c695166202d65498a0c968a7d3
Karel Zavadil <zavadil@incomaker.com>
2023-10-20 16:11:26 +0200

    removed vendor

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -4,7 +4,7 @@
 
 namespace Composer\Autoload;
 
-class ComposerStaticInit3a34061626024645c25863ffa23bee09
+class ComposerStaticInit2bedc9232bcb05f51ddf6fe84c850fe3
 {
     public static $prefixLengthsPsr4 = array (
         'I' => 
@@ -27,9 +27,9 @@ class ComposerStaticInit3a34061626024645c25863ffa23bee09
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInit3a34061626024645c25863ffa23bee09::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInit3a34061626024645c25863ffa23bee09::$prefixDirsPsr4;
-            $loader->classMap = ComposerStaticInit3a34061626024645c25863ffa23bee09::$classMap;
+            $loader->prefixLengthsPsr4 = ComposerStaticInit2bedc9232bcb05f51ddf6fe84c850fe3::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit2bedc9232bcb05f51ddf6fe84c850fe3::$prefixDirsPsr4;
+            $loader->classMap = ComposerStaticInit2bedc9232bcb05f51ddf6fe84c850fe3::$classMap;
 
         }, null, ClassLoader::class);
     }
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,22 +1,22 @@
 <?php return array(
     'root' => array(
+        'name' => 'incomaker/api',
         'pretty_version' => '1.0.0+no-version-set',
         'version' => '1.0.0.0',
+        'reference' => NULL,
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => NULL,
-        'name' => 'incomaker/api-core',
         'dev' => true,
     ),
     'versions' => array(
-        'incomaker/api-core' => array(
+        'incomaker/api' => array(
             'pretty_version' => '1.0.0+no-version-set',
             'version' => '1.0.0.0',
+            'reference' => NULL,
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => NULL,
             'dev_requirement' => false,
         ),
     ),
